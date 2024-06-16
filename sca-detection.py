dc24d5f04d5169dc1106069b38f584a056926520
Irtiza Ali <irtiza@stakater.com>
2024-06-16 20:28:47 +0200

    Update sca-detection.py

--- sca-detection.py
+++ sca-detection.py
@@ -5,7 +5,7 @@ f = open("sca.json")
 
 data = json.load(f)
 
-if ("indirect_vulnerabilities" in list(data)) and ((data["indirect_vulnerabilities"] > 0):
+if ("indirect_vulnerabilities" in list(data)) and ((data["indirect_vulnerabilities"] > 0)):
     raise Exception("SCA Tool detected issues")
 
 f.close()
